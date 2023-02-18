0b6a3f774699cc61ba3d14f03d83608eab9fc10f
Arun Upadhyay <arunnnet@gmail.com>
2023-02-18 09:11:53 -0500

    import export example

---
+++ import-export-exam/index.js
@@ -0,0 +1,6 @@
+import { funDiff, funDiv, funMul, funSum } from "./operations.js";
+
+console.log("Sum=" + funSum(1, 1));
+console.log("Differnence=" + funDiff(10, 1));
+console.log("Multiply=" + funMul(2, 5));
+console.log("Divide=" + funDiv(10, 2));
---
+++ import-export-exam/operations.js
@@ -0,0 +1,13 @@
+export const funSum = (a, b) => {
+  return a + b;
+};
+
+export const funDiff = (a, b) => {
+  return a - b;
+};
+export const funMul = (a, b) => {
+  return a * b;
+};
+export const funDiv = (a, b) => {
+  return a / b;
+};
